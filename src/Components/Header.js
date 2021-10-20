40a9f259a7cc3918962908012ce563088bc7d511
Hitallo Freitas <hitalloufpe@gmail.com>
2021-10-20 09:23:35 -0300

    header alinhado

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from './styles/Header.module.css';
+
 const Header = () => {
   return (
-    <div>
-      <nav>
+    <header className={styles.header}>
+      <nav className={`${styles.block} container `}>
         <NavLink to="/">HOME</NavLink>
         <NavLink to="game">GAME</NavLink>
         <NavLink to="training">TRAINING</NavLink>
         <NavLink to="comissaoorganizadora">COMISSÃO ORGANIZADORA</NavLink>
       </nav>
-    </div>
+    </header>
   );
 };
 
