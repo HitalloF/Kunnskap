d1077714a11bdc3afc7e99889c73f4c5272a80d0
Hitallo Freitas <hitalloufpe@gmail.com>
2021-10-20 09:02:26 -0300

    game

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Game from './Components/Game';
 import Header from './Components/Header';
 import Home from './Components/Home';
 
@@ -10,6 +11,7 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="game" element={<Game />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Components/Game.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Game = () => {
+  return (
+    <section>
+      <div>
+        <h1>Game</h1>
+        <p>
+          A primeira edição aconteceu virtualmente nos dias 06 a 09 de julho de
+          2020 no canal do KUNNSKAP no YouTube, contando com a participação de
+          14 universidades. A segunda edição ocorrou, nos dias 22 a 25 de
+          fevereiro de 2021 com a expectativa de participação de 15
+          universidades. Temos como nosso objetivo é transmitir conhecimento por
+          meio de um game online entre as universidades, onde os times formados
+          por estudantes de Engenharia de Minas deverão responder perguntas
+          relacionadas à mineração e na grande final, as equipes finalistas
+          deverão resolver um case. Em ambas edições contamos com o apoio e
+          patrocínio da Vale Mineração, Orica, ABREMI e ANBEM, e mais uma vez
+          queremos agradecer por confiar e apoiar o nosso projeto.
+        </p>
+      </div>
+      <div>
+        <h1>I Kunnskap GAME</h1>
+        <p>Veja agora como se sairam!</p>
+        VIDEOS!
+      </div>
+      <div>
+        <h1>II Kunnskap GAME</h1>
+        <p>Veja agora como se sairam!</p>
+        VIDEOS!
+      </div>
+    </section>
+  );
+};
+
+export default Game;
