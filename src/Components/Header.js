3b562090e14bd4307063a06c23dea05dbbae12dd
Hitallo Freitas <hitalloufpe@gmail.com>
2021-10-25 10:07:12 -0300

    menu mobile e ajuster

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './styles/Header.module.css';
 import logo from '../img/logo2.png';
+import useMedia from '../Hooks/useMedia';
 const Header = () => {
+  const mobile = useMedia('(max-width:40rem)');
+  const [mobileMenu, setMobileMenu] = React.useState(false);
+
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
+
   return (
     <header className={styles.header}>
       <div className={styles.boxnav}>
         <a href="/">
           <img src={logo} alt="" />
         </a>
-        <nav className={`${styles.block} container `}>
+        {mobile && (
+          <button
+            aria-label="Menu"
+            className={`${styles.mobileButton} ${
+              mobileMenu && styles.mobileButtonActive
+            }`}
+            onClick={() => setMobileMenu(!mobileMenu)}
+          ></button>
+        )}
+
+        <nav
+          className={`${mobile ? styles.navMobile : styles.block} ${
+            mobileMenu && styles.navMobileActive
+          } container `}
+        >
           <NavLink to="/">HOME</NavLink>
           <NavLink
             activeStyle={{
@@ -30,7 +54,7 @@ const Header = () => {
           >
             TRAINING
           </NavLink>
-          <NavLink
+          {/* <NavLink
             activeStyle={{
               color: '#263ca8',
               textShadow: '2px 2px 8px #ff7134',
@@ -38,7 +62,7 @@ const Header = () => {
             to="comissaoorganizadora"
           >
             COMISSÃO ORGANIZADORA
-          </NavLink>
+          </NavLink> */}
         </nav>
       </div>
     </header>
